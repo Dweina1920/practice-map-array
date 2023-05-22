cb0f2470c796f568e24924dff3bc6c28a0ec376f
Dina <dina.yamanid@gmail.com>
2023-05-22 13:26:00 +0200

    done

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 function doubleNumbers(arr) {
   // your code here
+  return arr.map((x) => x * 2);
 }
 
 console.log(doubleNumbers([2, 5, 100])); // [4, 10, 200]
 
 function stringItUp(arr) {
   // your code here
+  return arr.map((x) => `${x}`);
 }
 
 console.log(stringItUp([2, 5, 100])); // ["2", "5", "100"]
 
 function capitalizeNames(arr) {
+  let x = "uno";
   // your code here
+  return arr.map((x) => x[0].toUpperCase() + x.slice(1).toLowerCase());
 }
 
 console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"])); // ["John", "Jacob", "Jingleheimer", "Schmidt"]
 
 function namesOnly(arr) {
+  let nameObject = arr.map((x) => x.name.split(" ")[0]);
+  //let nombre = nameObject.map((x) => x.shift());
+
+  return nameObject;
+
   // your code here
 }
 
@@ -48,6 +57,10 @@ console.log(
 
 function makeStrings(arr) {
   // your code here
+  return arr.map(
+    (x) =>
+      `${x.name} ${x.age >= 18 ? "can go to The Matrix" : "is under age!!"}`
+  );
 }
 
 console.log(
@@ -81,6 +94,7 @@ console.log(
 // "Bob Ziroll can go to The Matrix"]
 
 function readyToPutInTheDOM(arr) {
+  return arr.map((x) => `<h1>${x.name}</h1><h2>${x.age}</h2>`);
   // your code here
 }
 console.log(
